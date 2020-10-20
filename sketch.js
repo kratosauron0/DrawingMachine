c1b65ba4fb19975d206670e97a593e5bf56885c3
jossraquel18245 <71808090+jossraquel18245@users.noreply.github.com>
2020-10-20 00:22:24 -0700

    Finished Drawing Machine
    Added HTML and CSS flare and finshed code for drawing.

--- sketch.js
+++ sketch.js
@@ -1,23 +1,48 @@
 let array = [];
+let images = [];
+let selected;
+
+function preload() {
+  // preload() runs once
+  for (let k = 0; k < 6; k++)
+  {
+    images[k] = loadImage(`assets/${k}.png`);
+  }
+}
+
 
 function setup() {
-  createCanvas(600, 600);
-  background(255, 134, 20);
+  createCanvas(800, 800);
+  background(240);
+
+  selected = int(random(images.length));
 
   strokeWeight(7);
   noFill();
+
 }
 
 function draw() {
 
-if (mouseIsPressed){
-  //line(mouseX, mouseY, pmouseX, pmouseY);
-  array.push([mouseX, mouseY]);
+  if (mouseIsPressed){
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
 
-  //stroke(map(mouseX, 0, 600, 0, 255, true));
-  //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY );
-  //line(mouseX, mouseY, pmouseX, pmouseY );
-}
+    //stroke(map(mouseX, 0, 600, 0, 255, true));
+    //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY );
+    //line(mouseX, mouseY, pmouseX, pmouseY );
+  }
+
+
+  image (images[selected],0,0);
+
+  beginShape();
+  for(let i = 0; i < array.length; i++){
+    //line(array[i][0], array [i][1], array[i + 1][0], array[i + 1][1]);
+    curveVertex(array[i][0], array [i][1])
+  }
+  endShape();
+
 
 }
 
@@ -28,17 +53,6 @@ function keyTyped(){
   if (key === 's'){
     // Save this image
     saveCanvas(`fileName`, 'png');
-  } else if (key === 'd'){
-    //display image
-
-    beginShape();
-    for(let i = 0; i < array.length; i++){
-      //line(array[i][0], array [i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array [i][1])
-    }
-    endShape();
-
-
   }
 
   return false;
