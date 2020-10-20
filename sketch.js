2d6b0fcea37ce6466acfe6adef4a39a60000a4e1
kratosauron0 <dragonsmashers@gmail.com>
2020-10-19 20:45:44 -0700

    Implement minor features
    Added some minor features, including an undo function and a mirroring function.

--- sketch.js
+++ sketch.js
@@ -1,45 +1,63 @@
 let array = [];
+let mirror = false;
 
-function setup() {
+function setup()
+{
   createCanvas(600, 600);
-  background(255, 134, 20);
+  background(200);
 
   strokeWeight(7);
   noFill();
 }
 
-function draw() {
-
-if (mouseIsPressed){
-  //line(mouseX, mouseY, pmouseX, pmouseY);
-  array.push([mouseX, mouseY]);
-
-  //stroke(map(mouseX, 0, 600, 0, 255, true));
-  //line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY );
-  //line(mouseX, mouseY, pmouseX, pmouseY );
-}
-
-}
-
-function keyTyped(){
-
-  //console.log(`key ${key} is being typed`)
-
-  if (key === 's'){
-    // Save this image
-    saveCanvas(`fileName`, 'png');
-  } else if (key === 'd'){
-    //display image
-
-    beginShape();
-    for(let i = 0; i < array.length; i++){
-      //line(array[i][0], array [i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array [i][1])
-    }
-    endShape();
-
-
+function draw()
+{
+  // Record user input as a drawing
+  if (mouseIsPressed)
+  {
+    array.push([mouseX, mouseY]);
   }
 
-  return false;
+  // Display the drawing
+  background(200);
+  beginShape();
+  for (let i = 0; i < array.length; i++)
+  {
+    curveVertex(array[i][0], array[i][1])
+  }
+  endShape();
+
+  if (mirror)
+  {
+    beginShape();
+    for (let i = 0; i < array.length; i++)
+    {
+      curveVertex(600 - array[i][0], array[i][1])
+    }
+    endShape();
+  }
+}
+
+function keyTyped()
+{
+  if (key === 's')
+  {
+    // Save this image
+    saveCanvas("myPicture", 'png');
+  }
+  else if (key === 'c')
+  {
+    array = [];
+  }
+  else if (key === 'u')
+  {
+    for (let i = 0; i < 10 && array.length > 0; i++)
+    {
+      array.pop();
+    }
+  }
+  else if (key === 'm')
+  {
+    mirror = !mirror;
+  }
 }
